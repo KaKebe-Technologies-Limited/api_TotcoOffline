b7af6ddf0f89908358a5db6a76f4aff3b4971a4f
unknown <lukengelukard@gmail.com>
2022-10-06 16:20:53 +0300

    added agent name

--- api/sales_orders/listAllSalesOrders.php
+++ api/sales_orders/listAllSalesOrders.php
@@ -5,7 +5,10 @@ include '../sanitizer.php';
 
 $response['success'] = 0;
 
-$sql1 = "SELECT * FROM tbl_sales_order WHERE isDeleted = '0'";
+$sql1 = "SELECT o.* , u.user_id, u.username Agent_Name, u.firstname, u.lastname
+FROM tbl_sales_order o
+JOIN tbl_user u ON o.createdBy= u.user_id
+WHERE isDeleted = '0'";
 
 $response_array = array();
 
@@ -20,7 +23,14 @@ if ($result1 = $conn->query($sql1)) {
             // so, for each row ---- $row1["sales_order_id"]
             $row_id = $row1["sales_order_id"];
 
-            $sql = "SELECT op.order_vs_pdt_id, op.sales_order_id, op.product_id, op.quantity, p.pdt_name FROM tbl_order_vs_product op JOIN tbl_sales_order o ON op.sales_order_id = o.sales_order_id JOIN tbl_product p ON op.product_id = p.product_id WHERE op.sales_order_id = '$row_id' ORDER BY op.order_vs_pdt_id DESC";
+            $sql = "SELECT op.order_vs_pdt_id, op.sales_order_id, op.product_id, op.quantity, p.pdt_name
+            FROM tbl_order_vs_product op 
+            JOIN tbl_sales_order o ON op.sales_order_id = o.sales_order_id 
+            JOIN tbl_product p ON op.product_id = p.product_id 
+             
+            WHERE op.sales_order_id = '$row_id' 
+            ORDER BY op.order_vs_pdt_id 
+            DESC";
 
             // Now, get the products under this sales order;
             if ($result = $conn->query($sql)) {
